48b3c0bedc69f56dd8430df8e6c8d6c6a776009b
StanleyYeh <yes0319@gmail.com>
2024-01-24 20:55:08 +0800

    新增:註冊/登入功能
    1. 建立Respository, 之後在Program.cs註冊
    2. 建立Controller
    3. 在appsetting添加secret key
    4. Bug : 在APIResponse 中 , 預先將ErrorMessages注入空字串

---
+++ MagicVilla_VillaAPI/Controllers/UsersController.cs
@@ -0,0 +1,68 @@
+﻿using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.DTO;
+using MagicVilla_VillaAPI.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/UserAuth")]
+    [ApiController]
+
+    public class UsersController : Controller
+    {
+        private readonly IUserRepository _userRepo;
+        protected APIResponse _response;
+
+        public UsersController(IUserRepository userRepo)
+        {
+            _userRepo = userRepo;
+            _response = new();
+            //this._response = new APIResponse();
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<APIResponse>> Login([FromBody] LoginRequestDTO model)
+        {
+            var loginResponse = await _userRepo.Login(model);
+
+            if(loginResponse.User == null || string.IsNullOrEmpty(loginResponse.Token) )
+            {
+                //return BadRequest("Username or Password is incorrect!");
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                _response.ErrorMessages.Add("Username or Password is incorrect!");
+                return BadRequest(_response);
+            }
+            _response.StatusCode = HttpStatusCode.OK;
+            _response.IsSuccess = true;
+            _response.Result = loginResponse;
+            return Ok(_response);
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<APIResponse>> Register([FromBody] RegisterRequestDTO model)
+        {
+            bool isUserNameUnique = _userRepo.IsUniqueUser(model.UserName);
+            if(!isUserNameUnique)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                _response.ErrorMessages.Add("Username already exists!");
+                return BadRequest(_response);
+            }
+
+            var user = await _userRepo.Register(model);
+            if(user == null)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                _response.ErrorMessages.Add("Error while registering!");
+                return BadRequest(_response);
+            }
+            _response.StatusCode = HttpStatusCode.OK;
+            _response.IsSuccess = true;
+            return Ok(_response);
+        }
+    }
+}
